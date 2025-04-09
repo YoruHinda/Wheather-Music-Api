2cb3b2fe118bbbd8a223676adc76d946026290bb
Ruan Phablo <ruanphablo778@gmail.com>
2025-04-09 17:57:48 -0300

    Feat: Add Files!

---
+++ src/main/java/com/github/yoruhinda/Wheather_Music_Api/WeatherMusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.yoruhinda.Wheather_Music_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMusicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/yoruhinda/Wheather_Music_Api/WheatherMusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yoruhinda.Wheather_Music_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WheatherMusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
