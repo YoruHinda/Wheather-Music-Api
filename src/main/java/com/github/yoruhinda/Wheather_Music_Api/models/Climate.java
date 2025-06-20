fb61ce12a1b06aea0a3f1ebfccc59affc7e15ff0
Ruan Phablo <ruanphablo778@gmail.com>
2025-06-19 23:11:50 -0300

    Chore: Create Methods!

---
+++ src/main/java/com/github/yoruhinda/Wheather_Music_Api/models/Climate.java
@@ -0,0 +1,5 @@
+package com.github.yoruhinda.Wheather_Music_Api.models;
+
+public enum Climate {
+    THUNDERSTORM, DRIZZLE, RAIN, SNOW, ATMOSPHERE, CLEAR, CLOUDS
+}
--- src/main/java/com/github/yoruhinda/Wheather_Music_Api/models/Music.java
+++ src/main/java/com/github/yoruhinda/Wheather_Music_Api/models/Music.java
@@ -1,4 +1,14 @@
 package com.github.yoruhinda.Wheather_Music_Api.models;
 
 public class Music {
+    private String genre;
+    private Climate climate;
+
+    public Music(Climate climate){
+
+    }
+
+    private void getHumorMusical(){
+
+    }
 }
